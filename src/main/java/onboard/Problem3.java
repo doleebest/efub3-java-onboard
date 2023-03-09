ceff419041e131a83a1cfe7864e4d7b960d3ac4e
Sunny Lee <80109963+sunnyineverywhere@users.noreply.github.com>
2023-03-09 11:35:49 +0900

    ✍🏻 docs: add Images
    - 깃허브 액션 확인 편리를 위한 이미지 추가

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -6,6 +6,23 @@ import java.util.List;
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
         List<Integer> answer = new ArrayList<>();
+        // average
+        int avg = 0;
+        int sum = 0;
+        for (int i=0; i<numbers.size(); i++){
+            sum += numbers.get(i);
+        }
+        avg = sum/numbers.size();
+        answer.add(avg);
+
+        //median
+        int max = numbers.get(0);
+        for (int i=0; i<numbers.size(); i++){
+            if(numbers.get(i)>max)
+                max = numbers.get(i);
+        }
+        answer.add(max);
+
         return answer;
     }
 }
